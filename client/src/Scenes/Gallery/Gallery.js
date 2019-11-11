3648be6ba36da319a8604f67b9164a1a0825a90a
Elvis Duru <duruelvis95@gmail.com>
2019-11-11 14:08:47 +0100

    changed gallery images

--- client/src/Scenes/Gallery/Gallery.js
+++ client/src/Scenes/Gallery/Gallery.js
@@ -7,6 +7,20 @@ import ContactBox from '../../components/ContactBox/ContactBox';
 import PhotoGallery from "react-photo-gallery";
 import Carousel, { Modal, ModalGateway } from "react-images";
 
+import img1 from '../../images/gallery/img_1.jpg';
+import img2 from '../../images/gallery/img_2.jpg';
+import img3 from '../../images/gallery/img_3.jpg';
+import img4 from '../../images/gallery/img_4.jpg';
+import img5 from '../../images/gallery/img_5.jpg';
+import img6 from '../../images/gallery/img_6.jpg';
+import img7 from '../../images/gallery/img_7.jpg';
+import img8 from '../../images/gallery/img_8.jpg';
+import img9 from '../../images/gallery/img_9.jpg';
+import img10 from '../../images/gallery/img_10.jpg';
+import img11 from '../../images/gallery/img_11.jpg';
+import img12 from '../../images/gallery/img_12.jpg';
+import img13 from '../../images/gallery/img_13.jpg';
+
 const Gallery = () => {
   const [currentImage, setCurrentImage] = useState(0);
   const [viewerIsOpen, setViewerIsOpen] = useState(false);
@@ -23,47 +37,67 @@ const Gallery = () => {
 
   const photos = [
     {
-      src: "https://source.unsplash.com/2ShvY8Lf6l0/800x599",
+      src: img1,
       width: 4,
       height: 3
     },
     {
-      src: "https://source.unsplash.com/Dm-qxdynoEc/800x799",
+      src: img2,
       width: 1,
       height: 1
     },
     {
-      src: "https://source.unsplash.com/qDkso9nvCg0/600x799",
+      src: img3,
       width: 3,
       height: 4
     },
     {
-      src: "https://source.unsplash.com/iecJiKe_RNg/600x799",
+      src: img4,
       width: 3,
       height: 4
     },
     {
-      src: "https://source.unsplash.com/epcsn8Ed8kY/600x799",
+      src: img5,
       width: 3,
       height: 4
     },
     {
-      src: "https://source.unsplash.com/NQSWvyVRIJk/800x599",
+      src: img6,
       width: 4,
       height: 3
     },
     {
-      src: "https://source.unsplash.com/zh7GEuORbUw/600x799",
+      src: img7,
       width: 3,
       height: 4
     },
     {
-      src: "https://source.unsplash.com/PpOHJezOalU/800x599",
+      src: img8,
       width: 4,
       height: 3
     },
     {
-      src: "https://source.unsplash.com/I1ASdgphUH4/800x599",
+      src: img9,
+      width: 4,
+      height: 3
+    },
+    {
+      src: img10,
+      width: 4,
+      height: 3
+    },
+    {
+      src: img11,
+      width: 3,
+      height: 4
+    },
+    {
+      src: img12,
+      width: 4,
+      height: 3
+    },
+    {
+      src: img13,
       width: 4,
       height: 3
     }
